0a63a61dc0042b4f5e04b1aaa47422fcd57b2091
afoo <afoo@afoo.me>
2022-03-17 13:05:51 +0800

    disable unused buttons for the time being

--- src/Settings.svelte
+++ src/Settings.svelte
@@ -8,6 +8,11 @@
 
     $: disableDefaultSaveFolder = !$showSaveFolder;
 
+    // TODO sync with store or remote in the future
+    let syncDisabled = true;
+    let importDisabled = true;
+    let exportDisabled = true;
+
     onMount(async () => {
 
     })
@@ -25,7 +30,7 @@
 
     <div class="flex justify-between items-center p-2 space-x-2 w-full mb-6">
         <div class="flex-none">
-            <button class="btn btn-sm btn-outline">
+            <button class="btn btn-sm btn-outline" disabled={syncDisabled}>
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                     <path d="M5.5 13a3.5 3.5 0 01-.369-6.98 4 4 0 117.753-1.977A4.5 4.5 0 1113.5 13H11V9.413l1.293 1.293a1 1 0 001.414-1.414l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13H5.5z"/>
                     <path d="M9 13h2v5a1 1 0 11-2 0v-5z"/>
@@ -38,8 +43,10 @@
 
         </div>
         <div class="flex-none">
-            <button class="btn btn-sm  btn-outline">Import</button>
-            <button class="btn btn-sm  btn-outline">Export</button>
+            <button class="btn btn-sm  btn-outline" data-tip="not done yet" disabled={importDisabled}>Import
+            </button>
+            <button class="btn btn-sm  btn-outline" data-tip="not done yet" disabled={exportDisabled}>Export
+            </button>
         </div>
     </div>
 
@@ -89,7 +96,7 @@
         </div>
         <div class="flex-none">
             <button class="btn btn-sm" on:click={()=> pop()}>OK</button>
-<!--            <button class="btn">Apply</button>-->
+            <!--            <button class="btn">Apply</button>-->
         </div>
     </div>
 </div>
